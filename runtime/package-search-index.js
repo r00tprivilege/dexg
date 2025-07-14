a9c4316255ebd6944123b3e06254e858adc845c4
Mohamad Aerabi <98118361+r00tprivilege@users.noreply.github.com>
2025-07-14 23:46:55 +0330

    Add files via upload

---
+++ runtime/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.guardsquare.dexguard.runtime.detection"},{"l":"com.guardsquare.dexguard.runtime.encryption"},{"l":"com.guardsquare.dexguard.runtime.net"}];updateSearchResults();
